5e12d1ee2044efe86f7017780ddbd1460a932c46
younggam <nainjoo2001@naver.com>
2023-05-17 21:42:54 +0900

    add comments

--- Assets/Scripts/ShieldHit.cs
+++ Assets/Scripts/ShieldHit.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 
 public class ShieldHit : MonoBehaviour
@@ -6,7 +5,7 @@ public class ShieldHit : MonoBehaviour
     private static readonly int hitPoint = Shader.PropertyToID("_HitPoint");
     private static readonly int hitTime = Shader.PropertyToID("_HitTime");
 
-    private MaterialPropertyBlock materialPropertyBlock;
+    private MaterialPropertyBlock materialPropertyBlock;//Object마다 다른 property를 적용해 줄 수 있게 함.
     private MeshRenderer meshRenderer;
 
     private void OnValidate()
@@ -17,7 +16,7 @@ public class ShieldHit : MonoBehaviour
     private void Start()
     {
         materialPropertyBlock = new MaterialPropertyBlock();
-        materialPropertyBlock.SetFloat(hitTime, -10);
+        materialPropertyBlock.SetFloat(hitTime, -10);//시작하자마자 파동이 생기지 않게.
         meshRenderer.SetPropertyBlock(materialPropertyBlock);
     }
 
@@ -25,6 +24,6 @@ public class ShieldHit : MonoBehaviour
     {
         materialPropertyBlock.SetVector(hitPoint, other.transform.position);
         materialPropertyBlock.SetFloat(hitTime, Time.time);
-        meshRenderer.SetPropertyBlock(materialPropertyBlock);
+        meshRenderer.SetPropertyBlock(materialPropertyBlock);//값이 바뀔 때 마다 호출해야함.
     }
 }
